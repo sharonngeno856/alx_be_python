1697ae6f7f3c340dbf608d685fe73fff48514268
Sharon Chepkemoi <sharonngeno23@gmail.com>
2025-08-03 14:58:46 +0300

    Fix test method names for calculator

---
+++ test_simple_calculator.py
@@ -0,0 +1,29 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 5), 4)
+        self.assertEqual(self.calc.add(-3, -3), -6)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-4, -2), -2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-3, 3), -9)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(5, 0), None)  # Division by zero
+        self.assertAlmostEqual(self.calc.divide(9, 4), 2.25)
+
+if __name__ == '__main__':
+    unittest.main()
